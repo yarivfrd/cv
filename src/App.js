9341a0ab673228b46292c0925d1794f9821dfb25
Yariv Fruend <yarivfrd@gmail.com>
2022-02-22 18:09:01 +0200

    Updated coming soon message

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ function App() {
   return (
     <main>
       <div className="inner">
-        <div className="title">
-          <div className="wrench">🔧</div>
-          COMING SOON
-          <div className="hammer">🔨</div>
-        </div>
+        <div className="title">COMING SOON</div>
         <div className="subtitle">cv.yariv.io</div>
       </div>
     </main>
